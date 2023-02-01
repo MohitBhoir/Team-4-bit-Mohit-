8060f71d2b9833637c425bee07f15584aa079188
unknown <mihirgrathod23@gmail.com>
2023-02-01 23:52:18 +0530

    backend

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use("/api/v1/history", auth, historyRouter)
 app.use(notFound)
 app.use(errohandlerMiddleware)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 
 
 const start = async () => {
